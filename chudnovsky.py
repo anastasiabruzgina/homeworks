b2bd506cda3c9dc58e0958d1e1796cef8207c01f
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2021-10-20 02:25:40 +0200

    ДЗ

---
+++ chudnovsky.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 01:42:38 2021
+
+@author: anast
+"""
+from decimal import Decimal
+x = 0.1 + 0.2 
+number = Decimal()
+
+print(number)
+x = 10005 + 0.02
+print(Decimal(x))
