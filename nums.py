7fe8cb03666dbdc4be7dd3a3e8db0141530fb82a
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2021-11-07 00:15:01 +0200

    Update nums.py

--- nums.py
+++ nums.py
@@ -5,4 +5,4 @@ nums = [randint(1, n) for i in range(1, n + 1)]
 def find_missing_nums(nums):
     normal_array = [i for i in range(1, n + 1)]
     a = list(filter(lambda i: i not in nums, normal_array))
-    return aa
+    return a
