8d8c58527949f0184678e232d9f53f073fa32f44
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2021-11-06 23:57:31 +0200

    пофиксила

--- nums.py
+++ nums.py
@@ -1,15 +1,10 @@
-import random
 from random import randint
 
-def find_missing_nums(nums):
-  norm = [d for d in range(1, len(nums)+1)]
-  a = []
-  for h in norm:
-    if h not in nums:
-      a.append(h)
-  return a
-
 n = int(input())
-nums = [randint(1, n) for i in range(n)]
-
-print(find_missing_nums(nums))
+nums = [randint(1, n) for i in range(1, n + 1)]
+def is_here(i):
+    return i not in nums
+def find_missing_nums(nums):
+    normal_array = [i for i in range(1, n + 1)]
+    a = list(filter(is_here, normal_array))
+    return a
