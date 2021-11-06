28ce9d8c7fcacbfe9615e5121eb060263f1355ab
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2021-11-07 00:14:33 +0200

    Update nums.py

--- nums.py
+++ nums.py
@@ -2,9 +2,7 @@ from random import randint
 
 n = int(input())
 nums = [randint(1, n) for i in range(1, n + 1)]
-def is_here(i):
-    return i not in nums
 def find_missing_nums(nums):
     normal_array = [i for i in range(1, n + 1)]
-    a = list(filter(is_here, normal_array))
-    return a
+    a = list(filter(lambda i: i not in nums, normal_array))
+    return aa
