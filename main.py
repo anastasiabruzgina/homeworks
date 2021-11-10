eb7855d36be0f87079dcbbee6d4d63a4a163ef09
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2021-11-11 00:08:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
-print("My name is...")
-"""
-Created on Wed Oct 13 19:22:31 2021
-
-@author: anast
-"""
-
+def is_monotonic(array):
+    def is_stonks(array):
+        i = 0
+        while i < len(array) - 1:
+            if array[i] <= array[i + 1]:
+                i += 1
+            else:
+                return False
+                break
+        return True
+    
+    def is_not_stonks(array):
+        i = 0
+        while i < len(array) - 1:
+            if array[i] >= array[i + 1]:
+                i += 1
+            else:
+                return False
+                break
+        return True
+    
+    return is_stonks(array) or is_not_stonks(array)
